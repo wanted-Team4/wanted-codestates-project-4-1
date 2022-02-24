973f51f73a50185b0cff85c8b30d5317da931639
DESKTOP-ICDGUQJ\하성화 <tjdghk3920@naver.com>
2022-02-24 13:43:01 +0900

    🎉 Start: 프로젝트 시작

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import React from "react";
+import Question1 from "./pages/Question1";
+import styled from "styled-components";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      {/* 여기에 네비게이션 두시면 됩니다. */}
+      <MainContainer>
+        <Routes>
+          <Route exact path="/" element={<Question1 />} />
+          {/* <Route exact path="/" element={<Index />} /> */}
+        </Routes>
+      </MainContainer>
+    </BrowserRouter>
+  );
+};
+const MainContainer = styled.main`
+    width:100%;
+    height: 100vh;
+    display:flex;
+    justify-content: left;
+    /* align-items: center; */
+`;
+export default App;
