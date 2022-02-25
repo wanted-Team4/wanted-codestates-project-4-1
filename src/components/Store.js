7c32b2198397805c369c0df6bc7a3b39c281b3fa
sunysty <seokjiseon@gmail.com>
2022-02-25 13:37:39 +0900

    🐛 Fix : Store 북마크 바로업데이트 추가

--- src/components/Store.js
+++ src/components/Store.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useCallback } from 'react';
 import { useRecoilState } from 'recoil';
 import { Repos } from '../atoms';
 import ContentBox from './ContentBox';
@@ -14,6 +14,15 @@ const Store = () => {
         setIsOn(!isOn);
     };
 
+    const [update, setUpdate] = useState(repoBookmark);
+    const forceUpdate = useCallback(() => setUpdate({}, []));
+
+    useEffect(() => {
+        forceUpdate();
+    }, [update]);
+
+    const bookmarkHandler = () => {};
+
     /*
     date: "2018-11-09T22:47:08Z"
     repo: "Delphi_Calculate"
@@ -22,28 +31,21 @@ const Store = () => {
     user: "HyoGeunGit"
     */
 
-    const bookmarkHandler = () => {
-        /*if (!saveRepos) {
-
-        } else {
-
-        }
-        */
-    };
-
     return (
-        <StoreWrapper>
+        <>
             {repoBookmark !== null ? (
                 repoBookmark &&
                 repoBookmark.map((repoItem, i) => (
-                    <Container key={i} onClick={() => {}}>
+                    <RepoListBox key={i} onClick={() => {}}>
                         <InfoBox>
                             <ImageBox>
                                 <img src={repoItem.url} alt='프로필' />
                             </ImageBox>
                             <TextBox>
                                 <Title>{repoItem.title}</Title>
-                                <Date>{repoItem.date}</Date>
+                                <Date>
+                                    update at {repoItem.date.split('T')[0]}
+                                </Date>
                             </TextBox>
                         </InfoBox>
 
@@ -59,29 +61,45 @@ const Store = () => {
                             <path d='M0 0h24v24H0z' fill='none' />
                             <path d='M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z' />
                         </svg>
-                    </Container>
+                    </RepoListBox>
                 ))
             ) : (
                 <>
                     <MessageBox>북마크에 Repository를 추가해보세요!</MessageBox>
                 </>
             )}
-        </StoreWrapper>
+        </>
     );
 };
 
 export default Store;
 
-const StoreWrapper = styled.div``;
-
-const Container = styled.div`
-    width: 100%;
-    border: 1px solid #444;
-    height: 100px;
+const RepoListBox = styled.div`
+    width: 90%;
+    height: 6.5em;
+    border-radius: 5px;
+    box-shadow: 5px 5px 7px 0px rgba(217, 217, 217, 1);
     display: flex;
-    align-items: center;
-    box-sizing: border-box;
+    justify-content: space-between;
+    padding: 0.8em 1em;
+    margin-left: 5%;
+    background-color: #ffffff;
+    margin-bottom: 1em;
+    transition: 0.2s;
+
+    :hover {
+        cursor: pointer;
+        transform: translateY(-3px);
+    }
+
+    > .checked {
+        fill: #fd9999;
+    }
+    > .unchecked {
+        fill: #cbc3c3;
+    }
 `;
+
 const InfoBox = styled.div`
     display: flex;
     align-items: center;
@@ -93,7 +111,7 @@ const ImageBox = styled.div`
     width: 3rem;
     height: 3rem;
     border-radius: 50%;
-    border: black solid 1px;
+    border: none;
     overflow: hidden;
 
     img {
@@ -103,11 +121,15 @@ const ImageBox = styled.div`
     }
 `;
 
-const TextBox = styled.div``;
+const TextBox = styled.div`
+    padding: 1rem;
+`;
 
 const Title = styled.p`
     margin-bottom: 0.2em;
     font-size: 1.1em;
+    color: #457cc7;
+    font-weight: bold;
 `;
 
 const Desc = styled.p`
