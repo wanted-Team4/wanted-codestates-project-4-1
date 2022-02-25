03646c831eaf59a541996cddd48b1fecadcda8c0
sunysty <seokjiseon@gmail.com>
2022-02-25 12:21:14 +0900

    ✨Feat : sotre componet 추가

---
+++ src/components/Store.js
@@ -0,0 +1,116 @@
+import React from 'react';
+import styled from 'styled-components';
+import { useEffect, useState } from 'react';
+import { useRecoilState } from 'recoil';
+import { Repos } from '../atoms';
+import ContentBox from './ContentBox';
+
+const Store = () => {
+    const [isOn, setIsOn] = useState(false);
+    let saveRepos = JSON.parse(localStorage.getItem('repoBookmark')); //클릭시 issue에 인자로 보내줌
+    console.log('>>>', saveRepos);
+
+    const toggleHandler = () => {
+        setIsOn(!isOn);
+    };
+
+    /*
+    date: "2018-11-09T22:47:08Z"
+    repo: "Delphi_Calculate"
+    title: "Delphi_Calculate"
+    url: "https://avatars.githubusercontent.com/u/35592924?v=4"
+    user: "HyoGeunGit"
+    */
+
+    const bookmarkHandler = () => {
+        /*if (!saveRepos) {
+
+        } else {
+
+        }
+        */
+        let saveRepos = JSON.parse(localStorage.getItem('repoBookmark'));
+    };
+
+    return (
+        <StoreWrapper>
+            {repoBookmark &&
+                repoBookmark.map((repoItem, i) => (
+                    <Container key={i} onClick={() => {}}>
+                        <InfoBox>
+                            <ImageBox>
+                                <img src={repoItem.url} alt='프로필' />
+                            </ImageBox>
+                            <TextBox>
+                                <Title>{repoItem.title}</Title>
+                                <Date>{repoItem.date}</Date>
+                            </TextBox>
+                        </InfoBox>
+
+                        <svg
+                            className={`${isOn ? 'checked' : 'unchecked'}`}
+                            onClick={bookmarkHandler}
+                            xmlns='http://www.w3.org/2000/svg'
+                            height='24px'
+                            viewBox='0 0 24 24'
+                            width='24px'
+                            key={i}
+                        >
+                            <path d='M0 0h24v24H0z' fill='none' />
+                            <path d='M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z' />
+                        </svg>
+                    </Container>
+                ))}
+        </StoreWrapper>
+    );
+};
+
+export default Store;
+
+const StoreWrapper = styled.div``;
+
+const Container = styled.div`
+    width: 100%;
+    border: 1px solid #444;
+    height: 100px;
+    display: flex;
+    align-items: center;
+    box-sizing: border-box;
+`;
+const InfoBox = styled.div`
+    display: flex;
+    align-items: center;
+    text-align: left;
+    max-width: 40em;
+`;
+
+const ImageBox = styled.div`
+    width: 3rem;
+    height: 3rem;
+    border-radius: 50%;
+    border: black solid 1px;
+    overflow: hidden;
+
+    img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+    }
+`;
+
+const TextBox = styled.div``;
+
+const Title = styled.p`
+    margin-bottom: 0.2em;
+    font-size: 1.1em;
+`;
+
+const Desc = styled.p`
+    font-size: 0.8em;
+    font-weight: 400;
+    margin-bottom: 0.2em;
+`;
+const Date = styled.p`
+    font-size: 0.8em;
+    font-weight: 400;
+`;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,23 +1,30 @@
-import { useEffect, useState } from "react";
-import styled from "styled-components";
-import Search from "../components/Search";
-import Issue from "../components/Issue";
+import { octokit } from '../utils/octokit';
+import { useEffect, useState } from 'react';
+import { useRecoilState } from 'recoil';
+import { Repos } from '../atoms';
+import styled from 'styled-components';
+import Issue from '../components/Issue';
+import Store from '../components/Store';
+import Search from '../components/Search';
 
-const MainContainer = styled.main`
-  width: 100%;
-  height: 100%;
+const Container = styled.div`
+    display: flex;
+`;
+const MainContainer = styled.div`
+    width: 70%;
+    height: 100%;
 `;
 
 const Navbar = styled.div`
-  width: 100%;
+    width: 100%;
 `;
 
 const LogoText = styled.p`
-  font-size: 2em;
-  font-weight: bold;
-  margin-left: 0.7em;
-  margin-top: 0.7em;
-  margin-bottom: 1em;
+    font-size: 2em;
+    font-weight: bold;
+    margin-left: 0.7em;
+    margin-top: 0.7em;
+    margin-bottom: 1em;
 `;
 
 const TabMenu = styled.ul`
@@ -35,60 +42,149 @@ const TabMenu = styled.ul`
         padding: 0.5rem;
         width: 100%;
         margin: 0 auto;
-        border-bottom: 1px solid #457CC7;
+        border-bottom: 1px solid #457cc7;
     }
 
     .focused {
-        border-bottom: 5px solid #457CC7;
+        border-bottom: 5px solid #457cc7;
     }
 `;
 
 const MenuContent = styled.li`
-  height: 100%;
+    height: 100%;
 `;
 
 const Desc = styled.div`
     text-align: center;
     padding: 2.5em 0 4em 0;
     font-weight: bold;
-    background-color: #f1f1f1;
-    height: 100%;
+`;
+
+const StoreContainer = styled.div`
+    width: 30%;
+    height: 100vh;
+    border-left: solid 1px #457cc7;
+
+    h2 {
+        font-size: 1.5em;
+        font-weight: bold;
+        width: 100%;
+        height: 155px;
+        display: flex;
+        justify-content: center;
+        align-items: flex-end;
+        padding: 1rem 0;
+    }
 `;
 
 const Home = () => {
-  let saveRepos = JSON.parse(localStorage.getItem("repoBookmark"));
-  const [currentTab, setCurrntTab] = useState(0)
+    const [repoData, setRepoData] = useState();
+    const [issueData, setIssueData] = useState();
+    const [repos, setRepos] = useRecoilState(Repos);
+    const [currentTab, setCurrntTab] = useState(0);
+    const [repoList, setRepoList] = useState([]);
 
-  const menuArr = [
-    { name: 'Search', content: <Search /> },
-    {
-      name: 'Issue',
-      content: <Issue user={saveRepos} />
+    const user = [
+        {
+            userName: 'songgao',
+            userRepo: 'water',
+        },
+        {
+            userName: 'balderdashy',
+            userRepo: 'waterline',
+        },
+    ];
+
+    const menuArr = [
+        {
+            name: 'Search',
+            content: <Search repoList={repoList} setRepoList={setRepoList} />,
+        },
+        {
+            name: 'Issue',
+            content: <Issue user={user}></Issue>,
+        },
+        {
+            name: 'Store',
+            content: <Store repoList={repoList}></Store>,
+        },
+    ];
+
+    // const menuArr = [
+    //   { name: 'Search', content: <Search repoList={repoList} setRepoList={setRepoList} /> },
+    //   { name: 'Issue', content: <Issue userName='songgao' userRepo='water'></Issue> }
+    // ];
+
+    const selectMenuHandler = (index) => {
+        setCurrntTab(index);
+    };
+
+    const addRepo = () => setRepos({ 1: '1' });
+
+    useEffect(() => {
+        searchRepo('water');
+        searchIssues('songgao', 'water');
+    }, []);
+
+    if (repoData) console.log(repoData[0].full_name.split('/'));
+    console.log(issueData);
+
+    return (
+        <Container>
+            <MainContainer>
+                <Navbar>
+                    <LogoText>PayHere</LogoText>
+
+                    <TabMenu>
+                        {menuArr.map((el, idx) => (
+                            <li
+                                key={idx}
+                                onClick={() => selectMenuHandler(idx)}
+                                className={
+                                    idx === currentTab
+                                        ? 'submenu focused'
+                                        : 'submenu'
+                                }
+                            >
+                                {el.name}
+                            </li>
+                        ))}
+                    </TabMenu>
+                </Navbar>
+                <Desc>
+                    <p>{menuArr[currentTab].content}</p>
+                </Desc>
+            </MainContainer>
+            <StoreContainer>
+                <h2>Public Repository</h2>
+                <Store repoList={repoList} />
+            </StoreContainer>
+        </Container>
+    );
+
+    async function searchRepo(q) {
+        await octokit
+            .request('GET /search/repositories', {
+                q: q,
+            })
+            .then((res) => {
+                const loadData = res.data.items;
+                setRepoData(loadData);
+            })
+            .catch((err) => alert(`에러 ${err}`));
     }
-  ];
 
-  const selectMenuHandler = (index) => {
-    setCurrntTab(index)
-  };
-
-  return (
-    <MainContainer>
-      <Navbar>
-        <LogoText>PayHere</LogoText>
-        <TabMenu>
-          {menuArr.map((el, idx) => (
-            <MenuContent
-              key={idx}
-              onClick={() => selectMenuHandler(idx)}
-              className={idx === currentTab ? 'submenu focused' : 'submenu'}
-            >{el.name}</MenuContent>
-          ))}
-        </TabMenu>
-      </Navbar>
-      <Desc>
-        {menuArr[currentTab].content}
-      </Desc>
-    </MainContainer>
-  );
-}
+    async function searchIssues(owner, repo) {
+        await octokit
+            .request('GET /repos/{owner}/{repo}/issues', {
+                owner: owner,
+                repo: repo,
+            })
+            .then((res) => {
+                const loadData = res.data;
+                setIssueData(loadData);
+            })
+            .catch((err) => alert(`에러 ${err}`));
+    }
+};
 export default Home;
