b1021d75d172aceb4855a2a3bbac916b1ea65144
sareum <rkdtkfma@gmail.com>
2022-02-25 19:18:54 +0900

    :bug: Fix : 토큰 오류 수정

--- src/utils/octokit.js
+++ src/utils/octokit.js
@@ -1,4 +1,4 @@
-import { Octokit } from "@octokit/rest"  
-export const octokit = new Octokit({     
-     auth: process.env.REACT_APP_GH,  
+import { Octokit } from "@octokit/rest"
+export const octokit = new Octokit({
+     // auth: process.env.REACT_APP_GH,
 });
